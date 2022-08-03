ca557fd90872aa0de4a93557e3caac0cd5cfae13
Connor <hessconnor41@gmail.com>
2022-08-03 12:11:37 +0200

    finish

--- create_config.py
+++ create_config.py
@@ -20,6 +20,15 @@ password = input("Password ['test1234']: ")
 # server_address = "192.168.1.2"
 config['DEFAULT']['password'] = password
 
+pfsense_config_path = input("Config File Full Path ['/dir' without config.php]: ")
+# server_address = "192.168.1.2"
+config['DEFAULT']['pfsense_config_path'] = pfsense_config_path
+
+
+pfsense_config_name = input("Config File Name ['config.php']: ")
+# server_address = "192.168.1.2"
+config['DEFAULT']['pfsense_config_name'] = pfsense_config_name
+
 
 # server_address = input("Server Address: ['example.com']")
 
--- pfsense/core.py
+++ pfsense/core.py
@@ -1,21 +1,68 @@
 from paramiko import SSHClient
 from pfsense.scp import SCPClient
 from pfsense import config
+import xmltodict
+import pprint
+from ast import literal_eval
+from dict2xml import dict2xml
+
 
 # config = configparser.ConfigParser()
 # config.read('../Config.ini')
 
 print("Core.py")
+pfsense_config_path = config['DEFAULT']['pfsense_config_path']
+pfsense_config_name = config['DEFAULT']['pfsense_config_name']
+pfsense_full_path = f"{pfsense_config_path}/{pfsense_config_name}"
+
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(config['DEFAULT']['server_address'], username=config['DEFAULT']['username'], password=config['DEFAULT']['password'])
 
+stdin, stdout, stderr = ssh.exec_command('8')
+
+
+cmd_output = stdout.read()
+print('log printing: ', cmd_output)
 # SCPCLient takes a paramiko transport as an argument
 scp = SCPClient(ssh.get_transport())
 
-# scp.put('test.txt', 'test2.txt')
-# scp.get('test2.txt')
+
+scp.get(pfsense_full_path)
+print("Read File")
+try:
+    keep_mac_list = literal_eval(config['DEFAULT']['exclude_mac'])
+except:
+    print("Error in exclude mac list in config file")
+    keep_mac_list = []
+
+new_passthrough_mac = []
+with open(pfsense_config_name, 'r') as f:
+    xml = (f.read())
+    my_dict = xmltodict.parse(xml)
+    passthrough_mac = my_dict['pfsense']['captiveportal']['cpbhswifi']['passthrumac']
+
+for item in passthrough_mac:
+    if item['mac'] in keep_mac_list:
+        new_passthrough_mac.append(item)
+
+# pprint.pprint(new_passthrough_mac)
+my_dict['pfsense']['captiveportal']['cpbhswifi']['passthrumac'] = new_passthrough_mac
+
+# pprint.pprint(my_dict['pfsense']['captiveportal']['cpbhswifi']['passthrumac'])
+
+new_xml = dict2xml(my_dict)
+print(new_xml)
+
+with open(pfsense_config_name, 'w') as f:
+    f.write(new_xml)
+
+print("Edited File")
+scp.put(pfsense_config_name, pfsense_full_path)
+print("Wrote File")
+
+
 
 # Uploading the 'test' directory with its content in the
 # '/home/user/dump' remote directory
