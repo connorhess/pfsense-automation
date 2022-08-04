08b7421b0964f26e74f8191f0731020ce2d43c79
Connor <hessconnor41@gmail.com>
2022-08-04 12:10:14 +0200

    added delay

--- pfsense/core.py
+++ pfsense/core.py
@@ -1,11 +1,11 @@
 from paramiko import SSHClient
 from pfsense.scp import SCPClient
 from pfsense import config
-import xmltodict
+# import xmltodict
 import pprint
 from ast import literal_eval
-from dict2xml import dict2xml
-
+# from dict2xml import dict2xml
+import xml.etree.ElementTree as ET
 
 # config = configparser.ConfigParser()
 # config.read('../Config.ini')
@@ -16,57 +16,66 @@ pfsense_config_name = config['DEFAULT']['pfsense_config_name']
 pfsense_full_path = f"{pfsense_config_path}/{pfsense_config_name}"
 
 
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(config['DEFAULT']['server_address'], username=config['DEFAULT']['username'], password=config['DEFAULT']['password'])
+def run():
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect(config['DEFAULT']['server_address'], username=config['DEFAULT']['username'], password=config['DEFAULT']['password'])
 
-stdin, stdout, stderr = ssh.exec_command('8')
-
-
-cmd_output = stdout.read()
-print('log printing: ', cmd_output)
-# SCPCLient takes a paramiko transport as an argument
-scp = SCPClient(ssh.get_transport())
+    stdin, stdout, stderr = ssh.exec_command('8')
 
 
-scp.get(pfsense_full_path)
-print("Read File")
-try:
-    keep_mac_list = literal_eval(config['DEFAULT']['exclude_mac'])
-except:
-    print("Error in exclude mac list in config file")
-    keep_mac_list = []
-
-new_passthrough_mac = []
-with open(pfsense_config_name, 'r') as f:
-    xml = (f.read())
-    my_dict = xmltodict.parse(xml)
-    passthrough_mac = my_dict['pfsense']['captiveportal']['cpbhswifi']['passthrumac']
-
-for item in passthrough_mac:
-    if item['mac'] in keep_mac_list:
-        new_passthrough_mac.append(item)
-
-# pprint.pprint(new_passthrough_mac)
-my_dict['pfsense']['captiveportal']['cpbhswifi']['passthrumac'] = new_passthrough_mac
-
-# pprint.pprint(my_dict['pfsense']['captiveportal']['cpbhswifi']['passthrumac'])
-
-new_xml = dict2xml(my_dict)
-print(new_xml)
-
-with open(pfsense_config_name, 'w') as f:
-    f.write(new_xml)
-
-print("Edited File")
-scp.put(pfsense_config_name, pfsense_full_path)
-print("Wrote File")
+    cmd_output = stdout.read()
+    print('log printing: ', cmd_output)
+    # SCPCLient takes a paramiko transport as an argument
+    scp = SCPClient(ssh.get_transport())
 
 
+    scp.get(pfsense_full_path)
+    print("Read File")
+    try:
+        keep_mac_list = literal_eval(config['DEFAULT']['exclude_mac'])
+    except:
+        print("Error in exclude mac list in config file")
+        keep_mac_list = []
 
-# Uploading the 'test' directory with its content in the
-# '/home/user/dump' remote directory
 
-# scp.put('test', recursive=True, remote_path='/home/user/dump')
+    tree = ET.parse(pfsense_config_name)
+    root = tree.getroot()
 
-scp.close()
+    for i in range(2):
+        for L1 in root:
+            if L1.tag == 'captiveportal':
+
+                for Parent in L1:
+                    if Parent.tag == 'cpbhswifi':
+
+                        for child in Parent:
+                            if child.tag == 'passthrumac':
+                                # print("child", child)
+
+                                for item in child:
+                                    if item.tag == 'mac':
+
+                                        item_mac = (item.text)
+                                        # print(item_mac)
+                                        if item_mac not in keep_mac_list:
+                                            Parent.remove(child)
+                        # print(item_mac)
+
+
+    tree.write(pfsense_config_name)
+
+
+
+    print("Edited File")
+    scp.put(pfsense_config_name, pfsense_full_path)
+    print("Wrote File")
+
+
+
+    # Uploading the 'test' directory with its content in the
+    # '/home/user/dump' remote directory
+
+    # scp.put('test', recursive=True, remote_path='/home/user/dump')
+
+    scp.close()
--- test_basic.py
+++ test_basic.py
@@ -1,3 +1,19 @@
-from pfsense import core
+import time
+from progress.bar import Bar
+from pfsense.core import run
 
+days = 7
+hours = days*24
+minutes = hours * 60
+seconds = minutes * 60
+seconds = 60
+
+for i in range(10):
+    # run()
+    bar = Bar('Sleeping', max=seconds)
+    for i in range(seconds):
+        # Do some work
+        bar.next()
+        time.sleep(1)
+    bar.finish()
 
